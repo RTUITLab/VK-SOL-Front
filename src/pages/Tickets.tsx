ebe4f4a296989abb3f48002b4718f0fe2f2ce3bf
cnrprod-team-27556 <sashalev200149@gmail.com>
2023-02-18 22:21:58 +0300

    Fix tickets

--- src/pages/Tickets.tsx
+++ src/pages/Tickets.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Cell, Group, List, Panel, PanelHeader, PanelHeaderBack, PanelHeaderContent, PanelProps, Spacing } from '@vkontakte/vkui'
+import { Cell, Group, List, Panel, PanelHeader, PanelHeaderBack, PanelHeaderContent, PanelProps, Spacing, Spinner } from '@vkontakte/vkui'
 import { back } from '@cteamdev/router'
 import Ticket from '../components/Ticket/Ticket'
 import { useQuery } from '@tanstack/react-query'
@@ -18,43 +18,26 @@ function Tickets({ nav }: PanelProps) {
         <PanelHeaderContent>Мои Билеты</PanelHeaderContent>
       </PanelHeader>
       {tickets.isLoading || events.isLoading
-        ? <>sad</>
+        ? <Spinner />
         : <Group>
           <List>
             {tickets.data.filter((item: any) => item.user_id == user.walletAddress).map((e: any) => {
               const t_event = events.data.find((i: any) => i._id == e.event_id)
               return (
-                <Cell key={e._id}>
-                  <Ticket
-                    image={e.url}
-                    eventName={t_event.name}
-                    date={t_event.date.split('T')[0]}
-                    time={t_event.date.split('T')[1]}
-                    address={t_event.place}
-                  />
-                </Cell>
+                <>
+                  <Cell key={e._id}>
+                    <Ticket
+                      image={e.url}
+                      eventName={t_event.name}
+                      date={t_event.date.split('T')[0]}
+                      time={t_event.date.split('T')[1]}
+                      address={t_event.place}
+                    />
+                  </Cell>
+                  <Spacing size={16} />
+                </>
               )
             })}
-            <Cell>
-              <Ticket
-                image='https://levandrovskiy.ru/img/foo.png'
-                eventName='Баста в Ледовом'
-                date='17 февраля 2023'
-                time='17:00'
-                address='Москва, пр. Вернадского, 78'
-              />
-            </Cell>
-            <Spacing size={16} />
-            <Cell>
-              <Ticket
-                image='https://levandrovskiy.ru/img/foo.png'
-                eventName='Баста не в Ледовом'
-                date='22 февраля 2023'
-                time='22:00'
-                address='Москва, пр. Вернадского, 78'
-              />
-            </Cell>
-            <Spacing size={16} />
           </List>
         </Group>}
     </Panel>
